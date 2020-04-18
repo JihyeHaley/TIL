5715f5f4f9584a2d91facd840163ea479a99696d
JihyeHaley <ohhojh@gmail.com>
2020-04-18 17:48:27 +0900

    study on Saturday

---
+++ R/lecture/day10.R
@@ -0,0 +1,346 @@
+# 날짜와 시간 관련 기능을 지원하는 함수들
+
+Sys.Date() #년, 월, 일
+Sys.time() #년, 월, 일, 시:분:초 국가
+
+class(Sys.Date())
+class(Sys.time())
+
+as.Date("2020-04-15")
+as.Date("2020/04/15")
+as.Date("2020,04,15")
+as.Date("15-04-2020")
+
+as.Date("2020,04,15", format="%Y,%m,%d")
+as.Date("15-04-2020", format="%d-%m-%Y")
+
+
+(today <- Sys.Date())
+format(today, "%Y년 %m월 %d일")
+format(today, "%d일 %B %Y년") #B - 월을 FULL NAME 으로
+format(today, "%y")
+format(today, "%Y")
+format(today, "%B")
+format(today, "%a")
+format(today, "%A")
+weekdays(today) 
+months(today) 
+quarters(today)
+unclass(today)  # 1970-01-01을 기준으로 얼마나 날짜가 지났지는 지의 값을 가지고 있다.
+Sys.Date()
+Sys.time()
+Sys.timezone()
+
+as.Date('1/15/2018',format='%m/%d/%Y') # format 은 생략 가능
+as.Date('4월 26, 2018',format='%B %d, %Y')
+as.Date('110228',format='%d%b%y') 
+
+x1 <- "2019-01-10 13:30:41"
+# 문자열을 날짜형으로
+as.Date(x1, "%Y-%m-%d %H:%M:%S") 
+# 문자열을 날짜+시간형으로
+strptime(x1, "%Y-%m-%d %H:%M:%S") # format 문자열 지정은 필수
+strptime('2019-08-21 14:10:30', "%Y-%m-%d %H:%M:%S")
+
+start <- as.Date("2020-01-01")
+end <- as.Date("2021-01-01")
+seq(start, end, 1)
+seq(start, end, "day")
+seq(start, end, "week")
+seq(start, end, "month")
+seq(start, end, "year")
+seq(start, end, "3 month")
+seq(start, end, length.out=7)
+
+
+x2 <- "20200601"
+as.Date(x2, "%Y%m%d")
+datetime<-strptime(x2, "%Y%m%d")
+str(datetime)
+
+# Date 객체는 날짜만 나타낼 수 있으며 시간처리 불가
+# 날짜와 시간을 함께 처리하려면 POSIXct 또는 POSIXlt 타입의 객체 사용
+
+pct <- as.POSIXct("2020/04/15 11:30:20")
+plt <- as.POSIXlt("2020/04/15 11:30:20")
+pct
+plt
+class(pct)
+class(plt)
+as.integer(pct)
+as.integer(plt)
+unclass(plt)
+plt$sec
+plt$min
+plt$hour
+plt$mday
+plt$mon # 0 - 1월
+plt$year
+plt$wday # 0-일요일
+
+
+
+t<-Sys.time()
+ct<-as.POSIXct(t)
+lt<-as.POSIXlt(t)
+str(ct) 
+str(lt) 
+unclass(ct) 
+unclass(lt) 
+lt$mon+1
+lt$hour
+lt$year+1900
+as.POSIXct(1449994438,origin="1970-01-01")
+as.POSIXlt(1449994438,origin="1970-01-01")
+
+
+#올해의 크리스마스 요일 2가지방법(요일명,숫자)
+christmas2<-as.POSIXlt("2020-12-25")
+weekdays(christmas2)
+christmas2$wday
+#2020년 1월 1일 어떤 요일
+tmp<-as.POSIXct("2020-01-01")
+weekdays(tmp)
+#오늘은 xxxx년x월xx일x요일입니다 형식으로 출력
+tmp<-Sys.Date()
+format(tmp,'오늘은 %Y년 %B %d일 %A입니다')
+year<-format(tmp,'%Y')
+month<-format(tmp,'%m')
+day<-format(tmp,'%d')
+weekday<-format(tmp,'%A')
+paste("오늘은 ",year,"년 ",month,"월 ",day,"일 ",weekday," 입니다.",sep="")
+
+as.Date("2020/01/01 08:00:00") - as.Date("2020/01/01 05:00:00")
+as.POSIXct("2020/01/01 08:00:00") - as.POSIXct("2020/01/01 05:00:00")
+as.POSIXlt("2020/01/01 08:00:00") - as.POSIXlt("2020/01/01 05:00:00")
+
+
+# 문자열 처리 관련 주요 함수들
+
+x <- "We have a dream"
+nchar(x)
+length(x)
+
+y <- c("We", "have", "a", "dream")
+length(y)
+nchar(y)
+
+letters
+sort(letters, decreasing=TRUE)
+
+fox.says <- "It is only with the HEART that one can See Rightly"
+tolower(fox.says)
+toupper(fox.says)
+
+substr("Data Analytics", start=1, stop=4)
+substr("Data Analytics", 6, 14)
+substring("Data Analytics", 6)
+
+classname <- c("Data Analytics", "Data Mining", "Data Visualization")
+substr(classname, 1, 4)
+
+countries <- c("Korea, KR", "United States, US", "China, CN")
+substr(countries, nchar(countries)-1, nchar(countries))
+
+head(islands)
+landmesses <- names(islands)
+landmesses
+grep(pattern="New", x=landmesses)
+
+index <- grep("New", landmesses)
+landmesses[index]
+# 동일
+grep("New", landmesses, value=T)
+
+
+txt <- "Data Analytics is useful. Data Analytics is also interesting."
+sub(pattern="Data", replacement="Business", x=txt)
+gsub(pattern="Data", replacement="Business", x=txt) #모두다 사용하고 싶을때
+
+x <- c("test1.csv", "test2.csv", "test3.csv", "test4.csv")
+gsub(".csv", "", x)
+
+words <- c("ct", "at", "bat", 
+           "chick", "chae", 
+           "cat", "cheanomeles", 
+           "chase", "chasse", 
+           "mychasse", "cheap", 
+           "check", "cheese", 
+           "hat", "mycat")
+
+grep("che", words, value=T)
+grep("at", words, value=T)
+grep("[ch]", words, value=T) #? c or h 
+grep("[at]", words, value=T) #? a or t
+grep("ch|at", words, value=T) # ch or at
+grep("ch(e|i)ck", words, value=T)
+grep("chase", words, value=T)
+grep("chas?e", words, value=T)
+grep("chas*e", words, value=T)
+grep("chas+e", words, value=T)
+grep("ch(a*|e*)se", words, value=T)
+grep("^c", words, value=T) # [^] => NOT, ^ => ~로 시작하고
+grep("t$", words, value=T) # $ => ~로 끝나고
+grep("^c.*t$", words, value=T)
+
+words2 <- c("12 Dec", "OK", "http//", 
+            "<TITLE>Time?</TITLE>", 
+            "12345", "Hi there")
+
+grep("[[:alnum:]]", words2, value=TRUE)
+grep("[[:alpha:]]", words2, value=TRUE)
+grep("[[:digit:]]", words2, value=TRUE)
+grep("[[:punct:]]", words2, value=TRUE)
+grep("[[:space:]]", words2, value=TRUE)
+grep("\\w", words2, value=TRUE)
+grep("\\d", words2, value=TRUE)
+grep("\\s", words2, value=TRUE)
+
+
+
+fox.said <- "What is essential is invisible to the eye"
+fox.said
+strsplit(x= fox.said, split= " ") #list 소환
+strsplit(x= fox.said, split="")
+
+fox.said.words <- unlist(strsplit(fox.said, " "))
+fox.said.words
+fox.said.words <- strsplit(fox.said, " ")[[1]]
+fox.said.words
+fox.said.words[3]
+p1 <- "You come at four in the afternoon, than at there I shall begin to the  happy"
+p2 <- "One runs the risk of weeping a little, if one lets himself be tamed"
+p3 <- "What makes the desert beautiful is that somewhere it hides a well"
+littleprince <- c(p1, p2, p3)
+strsplit(littleprince, " ")
+strsplit(littleprince, " ")[[3]] 
+strsplit(littleprince, " ")[[3]][5]
+
+
+
+
+# dplyr 패키지를 학습하자....
+
+install.packages("dplyr") 
+library(dplyr)
+install.packages("ggplot2")
+str(ggplot2::mpg)
+head(ggplot2::mpg)
+mpg <- as.data.frame(ggplot2::mpg)
+head(mpg)
+exam <- read.csv("data/csv_exam.csv")
+View(exam)
+getwd()
+setwd('C:/HaleyDeveloper/Rstudy')
+str(exam)
+dim(exam)
+head(exam);tail(exam)
+View(exam)
+# exam에서 class가 1인 경우만 추출하여 출력
+exam %>% filter(class == 1) # [참고] 단축키 [Ctrl+Shit+M]으로 %>% 기호 입력
+# 2반인 경우만 추출
+exam %>% filter(class == 2)
+# 1반이 아닌 경우
+exam %>% filter(class != 1)
+# 3반이 아닌 경우
+exam %>% filter(class != 3)
+# 수학 점수가 50점을 초과한 경우
+exam %>% filter(math > 50)
+# 수학 점수가 50점 미만인 경우
+exam %>% filter(math < 50)
+# 영어점수가 80점 이상인 경우
+exam %>% filter(english >= 80)
+# 영어점수가 80점 이하인 경우
+exam %>% filter(english <= 80)
+# 1반 이면서 수학 점수가 50점 이상인 경우
+exam %>% filter(class == 1 & math >= 50)
+# 2반 이면서 영어점수가 80점 이상인 경우
+exam %>% filter(class == 2 & english >= 80)
+# 수학 점수가 90점 이상이거나 영어점수가 90점 이상인 경우
+exam %>% filter(math >= 90 | english >= 90)
+# 영어점수가 90점 미만이거나 과학점수가 50점 미만인 경우
+exam %>% filter(english < 90 | science < 50)
+# 목록에 해당되는 행 추출하기
+exam %>% filter(class == 1 | class == 3 | class == 5)  # 1, 3, 5 반에 해당되면 추출
+# %in% 연산자 이용하기
+exam %>% filter(class %in% c(1,3,5))  # 1, 3, 5 반에 해당하면 추출
+# 추출한 행으로 데이터 만들기
+class1 <- exam %>% filter(class == 1)  # class가 1인 행 추출, class1에 할당
+class2 <- exam %>% filter(class == 2)  # class가 2인 행 추출, class2에 할당
+mean(class1$math)                      # 1반 수학 점수 평균 구하기
+mean(class2$math)                      # 2반 수학 점수 평균 구하기
+
+
+exam %>% select(math)  # math 추출
+exam %>% select(english)  # english 추출
+# 여러 변수 추출하기
+exam %>% select(class, math, english)  # class, math, english 변수 추출
+# 변수 제외하기
+exam %>% select(-math)  # math 제외
+exam %>% select(-math, -english)  # math, english 제외
+# dplyr 함수 조합하기
+# class가 1인 행만 추출한 다음 english 추출
+exam %>% filter(class == 1) %>% select(english) # 1반 학생의 영어성적만 보여줘
+# 가독성 있게 줄 바꾸기
+exam %>%
+  filter(class == 1) %>%  # class가 1인 행 추출
+  select(english)         # english 추출
+# 일부만 출력하기
+exam %>%
+  select(id, math) %>%  # id, math 추출
+  head                  # 앞부분 6행까지 추출
+# 일부만 출력하기
+exam %>%
+  select(id, math) %>%  # id, math 추출
+  head(10)              # 앞부분 10행까지 추출
+
+iris %>% pull(Species) #벡터로 꺼내줌
+iris %>% select(Species) 
+iris %>% select_if(is.numeric) 
+iris %>% select(-Sepal.Length, -Petal.Length)
+
+# Select column whose name starts with "Petal"
+iris %>% select(starts_with("Petal"))
+
+# Select column whose name ends with "Width"
+iris %>% select(ends_with("Width"))
+
+# Select columns whose names contains "etal"
+iris %>% select(contains("etal"))
+
+# Select columns whose name maches a regular expression
+iris %>% select(matches(".t."))
+
+
+# 오름차순으로 정렬하기
+exam %>% arrange(math)  # math 오름차순 정렬
+# 내림차순으로 정렬하기
+exam %>% arrange(desc(math))  # math 내림차순 정렬
+# 정렬 기준 변수 여러개 지정
+exam %>% arrange(desc(class), desc(math))  # class 및 math 오름차순 정렬
+exam %>% arrange(desc(math)) %>% head(1)
+
+exam %>%
+  mutate(total = math + english + science) %>%  # 총합 변수 추가
+  head                                          # 일부 추출
+#여러 파생변수 한 번에 추가하기
+exam %>%
+  mutate(total = math + english + science,          # 총합 변수 추가
+         mean = (math + english + science)/3) %>%   # 총평균 변수 추가
+  head     
+exam %>%
+  mutate(total = math + english + science,          # 총합 변수 추가
+         mean = total/3) %>%   # 총평균 변수 추가
+  head 
+
+# 일부 추출
+# mutate()에 ifelse() 적용하기
+exam %>%
+  mutate(test = ifelse(science >= 60, "pass", "fail")) %>%
+  head
+#추가한 변수를 dplyr 코드에 바로 활용하기
+exam %>%
+  mutate(total = math + english + science) %>%  # 총합 변수 추가
+  arrange(total) %>%                            # 총합 변수 기준 정렬
+  head                                          # 일부 추출
+
---
+++ R/lecture/day8.R
@@ -0,0 +1,188 @@
+# SNS의 Open API 활용
+library(httr)
+library(rvest)
+library(XML)
+library(httr)
+rm(list=ls())
+searchUrl<- "https://openapi.naver.com/v1/search/blog.xml"
+Client_ID <- "izGsqP2exeThwwEUVU3x"
+Client_Secret <- "WrwbQ1l6ZI"
+
+query <- URLencode(iconv("봄","euc-kr","UTF-8"))
+url <- paste0(searchUrl, "?query=", query, "&display=100")
+doc <- GET(url, add_headers
+           ("Content_Type" = "application/xml",
+            "X-Naver-client-Id" = Client_ID, 
+            "X-naver-Client-Secret" = Client_Secret))
+
+# 블로그 내용에 대한 리스트 만들기		
+doc2 <- htmlParse(doc, encoding="UTF-8")
+text<- xpathSApply(doc2, "//item/description", xmlValue)
+text
+text <- gsub("</?b>", "", text)
+text <- gsub("&.+t;", "", text)
+text
+
+# 네이버 뉴스 연동  
+searchUrl<- "https://openapi.naver.com/v1/search/news.xml"
+Client_ID <- "izGsqP2exeThwwEUVU3x"
+Client_Secret <- "WrwbQ1l6ZI"
+query <- URLencode(iconv("코로나","euc-kr","UTF-8"))
+url <- paste0(searchUrl, "?query=", query, "&display=100")
+doc <- GET(url, add_headers("Content_Type" = "application/xml",
+                            "X-Naver-client-Id" = Client_ID, "X-naver-Client-Secret" = Client_Secret))
+
+# 네이버 뉴스 내용에 대한 리스트 만들기		
+doc2 <- htmlParse(doc, encoding="UTF-8")
+text<- xpathSApply(doc2, "//item/description", xmlValue); 
+text
+text <- gsub("</?b>", "", text)
+text <- gsub("&.+t;", "", text)
+text
+
+
+# 트위터 글 읽어오기
+install.packages("rtweet")
+library(rtweet) 
+appname <- "edu_data_collection"
+api_key <- "RvnZeIl8ra88reu8fm23m0bST"
+api_secret <- "wTRylK94GK2KmhZUnqXonDaIszwAsS6VPvpSsIo6EX5GQLtzQo"
+access_token <- "959614462004117506-dkWyZaO8Bz3ZXh73rspWfc1sQz0EnDU"
+access_token_secret <- "rxDWfg7uz1yXMTDwijz0x90yWhDAnmOM15R6IgC8kmtTe"
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = api_key,
+  consumer_secret = api_secret,
+  access_token = access_token,
+  access_secret = access_token_secret)
+
+key <- "코로나"
+key <- enc2utf8(key)
+result <- search_tweets(key, n=500, token = twitter_token)
+str(result)
+result$retweet_text
+content <- result$retweet_text
+content <- gsub("[[:lower:][:upper:][:digit:][:punct:][:cntrl:]]", "", content)   
+content
+
+
+library(XML)
+API_key  <- "%2BjzsSyNtwmcqxUsGnflvs3rW2oceFvhHR8AFkM3ao%2Fw50hwHXgGyPVutXw04uAXvrkoWgkoScvvhlH7jgD4%2FRQ%3D%3D"
+bus_No <- "402"
+url <- paste("http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?ServiceKey=", API_key, "&strSrch=", bus_No, sep="")
+doc <- xmlParse(url, encoding="UTF-8")
+top <- xmlRoot(doc)
+top
+df <- xmlToDataFrame(getNodeSet(doc, "//itemList"))
+df
+str(df)
+
+busRouteId <- df$busRouteId
+busRouteId
+
+
+
+
+url <- paste("http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?ServiceKey=", API_key, "&busRouteId=", busRouteId, sep="")
+doc <- xmlParse(url, encoding="UTF-8")
+top <- xmlRoot(doc)
+top
+df <- xmlToDataFrame(getNodeSet(doc, "//itemList"))
+df
+
+# 서울시 빅데이터- XML 응답 처리
+# http://openapi.seoul.go.kr:8088/796143536a756e69313134667752417a/xml/LampScpgmtb/1/100/
+
+library(XML)
+key = '796143536a756e69313134667752417a'
+contentType = 'xml'
+startIndex = '1'
+endIndex = '200'
+url = paste0('http://openapi.seoul.go.kr:8088/',key,'/',contentType,'/LampScpgmtb/',startIndex,'/',endIndex,'/')
+
+con <- url(url, "rb") 
+imsi <- read_html(con)
+t <- htmlParse(imsi, encoding="UTF-8")
+upNm<- xpathSApply(t,"//row/up_nm", xmlValue) 
+pgmNm<- xpathSApply(t,"//row/pgm_nm", xmlValue)
+targetNm<- xpathSApply(t,"//row/target_nm", xmlValue)
+price<- xpathSApply(t,"//row/u_price", xmlValue)
+
+df <- data.frame(upNm, pgmNm, targetNm, price)
+View(df)
+write.csv(df, "edu.csv")
+
+# 한국은행 결제 통계시스템 Open API - JSON 응답 처리
+library(jsonlite)
+key = '/4WQ7X833TXC370SUTDX4/'
+contentType = 'json/'
+startIndex = '1'
+endIndex = '/100/'
+
+url <- paste0('http://ecos.bok.or.kr/api/KeyStatisticList',key,contentType,'kr/',startIndex,endIndex)
+response <- GET(url)
+json_data <- content(response, type = 'text', encoding = "UTF-8")
+json_obj <- fromJSON(json_data)
+df <- data.frame(json_obj)
+df <- df[-1]
+names(df) <- c("className", "unitName", "cycle", "keystatName", "dataValue")
+View(df)
+
+
+
+# apply 계열의 함수를 알아보자
+weight <- c(65.4, 55, 380, 72.2, 51, NA)
+height <- c(170, 155, NA, 173, 161, 166)
+gender <- c("M", "F","M","M","F","F")
+
+df <- data.frame(w=weight, h=height)
+df
+
+apply(df, 1, sum, na.rm=TRUE) #na.rm 은 sum한테 전달이 된다.
+apply(df, 2, sum, na.rm=TRUE) #? 1,2??
+lapply(df, sum, na.rm=TRUE) #list로 리턴 열단위로
+sapply(df, sum, na.rm=TRUE) #vector로 리턴 
+tapply(1:6, gender, sum, na.rm=TRUE)
+tapply(df$w, gender, mean, na.rm=TRUE)
+mapply(paste, 1:5, LETTERS[1:5], month.abb[1:5])
+
+v<-c("abc", "DEF", "TwT")
+sapply(v, function(d) paste("-",d,"-", sep="")) #named vector
+
+l<-list("abc", "DEF", "TwT")
+sapply(l, function(d) paste("-",d,"-", sep="")) #vector로 리턴
+lapply(l, function(d) paste("-",d,"-", sep="")) #그때 그때 다른 return
+
+flower <- c("rose", "iris", "sunflower", "anemone", "tulip")
+length(flower)
+nchar(flower) #문자길이
+sapply(flower, function(d) if(nchar(d) > 5) return(d)) #마지막으로 수행한 값이 결과로 리턴
+sapply(flower, function(d) if(nchar(d) > 5) d)
+sapply(flower, function(d) if(nchar(d) > 5) return(d) else return(NA))
+sapply(flower, function(d) paste("-",d,"-", sep=""))
+sapply(flower, function(d, n) if(nchar(d) > n) return(d), 2)
+
+count <- 1
+myf <- function(x, wt=T){
+  print(paste(x,"(",count,")"))
+  if(wt)
+    r <- paste("*", x, "*")
+  else
+    r <- paste("#", x, "#")
+  count <<- count + 1;
+  return(r)
+}
+result <- sapply(df$w, myf)
+length(result)
+result
+sapply(df$w, myf, F)
+sapply(df$w, myf, wt=F)
+rr1 <- sapply(df$w, myf, wt=F)
+str(rr1)
+
+count <- 1
+sapply(df, myf)
+rr2 <- sapply(df, myf)
+str(rr2)
+rr2[1,1]
+rr2[1,"w"]
