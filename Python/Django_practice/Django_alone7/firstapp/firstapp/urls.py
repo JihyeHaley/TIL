38a85d4481e8d736d5e30ea9dac1c8e3f41d5b5f
JihyeHaley <ohhojh@gmail.com>
2020-06-11 15:48:20 +0900

    succed self creating django framework

--- Python/Django_practice/Django_alone7/firstapp/firstapp/urls.py
+++ Python/Django_practice/Django_alone7/firstapp/firstapp/urls.py
@@ -1,4 +1,4 @@
-"""myapp URL Configuration
+"""firstapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from articles import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
