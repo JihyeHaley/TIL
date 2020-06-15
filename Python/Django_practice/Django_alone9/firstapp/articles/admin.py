ff2a8565a8d148a667e7c3cb3273bd765500780c
JihyeHaley <ohhojh@gmail.com>
2020-06-15 17:53:33 +0900

    happy to make it

--- Python/Django_practice/Django_alone9/firstapp/articles/admin.py
+++ Python/Django_practice/Django_alone9/firstapp/articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'content', 'created_at', 'updated_at', 'test']
+    list_editable = ['title']
+admin.site.register(Article, ArticleAdmin)
+# admin site에 등록(register)하겠다.
