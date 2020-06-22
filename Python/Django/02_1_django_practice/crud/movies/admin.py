c097b64ab89850b4620573dd89adedb005894f90
JihyeHaley <ohhojh@gmail.com>
2020-06-22 13:34:30 +0900

    commit class studying

--- Python/Django/02_1_django_practice/crud/movies/admin.py
+++ Python/Django/02_1_django_practice/crud/movies/admin.py
@@ -5,5 +5,6 @@ from .models import Movie
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'title_en', 'audience', 'open_date', 'genre', 'watch_grade', 'score', 'poster_url', 'description']
-
+    # 소괄호를 쓴 이유는 수정할 없어서서 
+    
 admin.site.register(Movie, MovieAdmin)
