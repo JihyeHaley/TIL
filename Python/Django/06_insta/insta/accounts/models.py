53dfdbdc0bc465d73b7c14776e3f9826001cdcef
JihyeHaley <ohhojh@gmail.com>
2020-06-26 14:21:11 +0900

    learn something complicated2

--- Python/Django/06_insta/insta/accounts/models.py
+++ Python/Django/06_insta/insta/accounts/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-# from django.contrib.auth import get_user_model
-# Create your models here.
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 
 class User(AbstractUser):
     following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='follower')
-    pass
+    
+    image = ProcessedImageField(
+        upload_to='profile_image',
+        processors=[ResizeToFill(500, 500)],
+        format='JPEG',
+        options={
+            'quality': 60},
+        default='default.png')
     # user_set이 새로 칼럼 생성되서 이름을 follower로 변화
     # 필수로 넣어두고 작업을 하기 :)
