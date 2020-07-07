917b9927438a5c01e1ea1aa27ed1b277fa8ae8b0
JihyeHaley <ohhojh@gmail.com>
2020-07-07 16:40:48 +0900

    project commit

--- Python/python/03_slicing.py
+++ Python/python/03_slicing.py
@@ -4,7 +4,7 @@
 print('----slicing-----')
 sample_list = list(range(0,31))
 print(sample_list)
-print(sample_list[1:3])
+print(sample_list[1:4])
 print(sample_list[10:-1])
 print(sample_list[0:len(sample_list):3])
 print('----- slicing 중간 생략 -----')
