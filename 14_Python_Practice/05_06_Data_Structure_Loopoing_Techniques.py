3953dcceacf431b10fe631734ff0cdf64cccce24
JihyeHaley <ohhojh@gmail.com>
2020-08-09 15:21:32 +0900

    study python tutorial 05_06_Data_Structure_Looping_Techniques

--- 14_Python_Practice/05_06_Data_Structure_Loopoing_Techniques.py
+++ 14_Python_Practice/05_06_Data_Structure_Loopoing_Techniques.py
@@ -11,3 +11,25 @@ questions = ['name', 'quest', 'favorite color']
 answers = ['jihye', 'the holy let see...', 'purple']
 for q, a in zip(questions, answers):
     print('What is your {}? \tIt is {}\n' .format(q, a))
+
+for i in reversed(range(1, 10, 2)):
+    print(i)
+
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+# set을 하면 겹치는건 merge? 되기 때문에
+basket_list = []
+for f in sorted(set(basket)):
+    basket_list.append(f)
+print(basket_list)
+
+
+import math 
+raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
+filtered_data= []
+for value in raw_data:
+    if not math.isnan(value):
+        filtered_data.append(value)
+
+print(filtered_data)
+
+print(float('NaN')) # nan
