3fee574700a660fed95f7c6414cff0398ff29a0d
JihyeHaley <ohhojh@gmail.com>
2020-08-11 14:47:41 +0900

    study python tutorial 07_00_Input_And_Output_Preview

---
+++ 14_Python_Practice/07_00_Input_And_Output.py
@@ -0,0 +1,4 @@
+##  There are several ways to present the output of a program; 
+# data can be printed in a human-readable form, or written to a file for future use. 
+# This chapter will discuss some of the possibilities.
+
