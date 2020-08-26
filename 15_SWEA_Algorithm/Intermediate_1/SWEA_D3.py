d1872241409233f88b1d371891a012233f824b38
JihyeHaley <ohhojh@gmail.com>
2020-08-26 22:43:37 +0900

    solve how to find demical

--- 15_SWEA_Algorithm/Intermediate_1/SWEA_D3.py
+++ 15_SWEA_Algorithm/Intermediate_1/SWEA_D3.py
@@ -2,10 +2,21 @@ T = int(input())
 
 def name(T):
     split = []
+    comma = 'last'
     for i in range(0, T):
-        # 문장의 수 
         n = int(input())
         sentence = str(input())
-        split.append(sentence.split('.'))
-        split.append(sentence.split('!'))
-        split.append(sentence.split('?'))
+        split.append(sentence.split(' '))
+        for j in range(0, len(split)):
+            word_len = len(split[i]) - 1 
+            if split[j][word_len] == '!':
+                comma = '!'
+            elif split[j][word_len] == '.':
+                comma = '.'
+            elif split[j][word_len] == '?':
+                comma = '?'
+            else:
+                pass
+                
+            if split[i][word_len] == '!' or '?' or '.':
+                split.append(sentence.split())
