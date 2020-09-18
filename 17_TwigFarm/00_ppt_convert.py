d3dc486dd807ee496ddf18140e7c6532c065d4ff
JihyeHaley <ohhojh@gmail.com>
2020-09-18 23:39:36 +0900

    try test code

---
+++ 17_TwigFarm/00_ppt_convert.py
@@ -0,0 +1,27 @@
+import os
+
+path = '/Users/haley/Desktop/Git/TIL/17_TwigFarm/ppt'
+file_list = os.listdir(path)
+
+
+from pptx import Presentation
+
+results = []
+
+for file_name_raw in file_list:
+
+    result = []
+    result.append(file_name_raw)
+
+    file_name = './ppt/' + file_name_raw
+    prs = Presentation(file_name)
+
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                result.append(paragraph.text)
+    results.append(result)
+
+print(results)
