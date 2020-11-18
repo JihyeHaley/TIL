c08084a1f2c4cfbf81998af970ab30360056e260
JihyeHaley <ohhojh@gmail.com>
2020-11-18 11:03:10 +0900

    push for tag finder

--- 22_Tag/TM_Friend.py
+++ 22_Tag/TM_Friend.py
@@ -1,5 +1,8 @@
 from raw_data_to_list import call_tagMT_ko_simple_lan
+from konlpy.tag import Mecab
+mecab = Mecab()
 
 # test용으로 simple의 한글 먼저 받아와서 돌리기
 ko_list = call_tagMT_ko_simple_lan('ko')
-print(ko_list)
+for _ in ko_list:
+    print(mecab.pos(_))
--- 22_Tag/test.py
+++ 22_Tag/test.py
@@ -1,15 +1,36 @@
-# from konlpy.tag import Mecab
+from raw_data_to_list import call_tagMT_ko_simple_lan
+from konlpy.tag import Mecab
+import xlsxwriter
+from datetime import datetime
+import timeit
 
-# mecab = Mecab()
-# sent = '안녕하세요, 지혜입니다.'
-# sent = mecab.pos(sent)
 
-import pandas as pd
-tm_raw_data = pd.read_excel('./tagMT_ko_simpl.xlsx')
-ko_list = tm_raw_data['ko'].tolist()
-en_list = tm_raw_data['en'].tolist()
-ja_list = tm_raw_data['ja'].tolist()
+timestamp = datetime.now().strftime('%m%d%H%M')
+workbook = xlsxwriter.Workbook('./test_' + timestamp + '_.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write('A1' , 'Raw TM')
+worksheet.write('B1' , 'Pos Tag')
 
+row_idx = 2
+
+
+# 엑셀 인덱스 작업
+def excel_index_creator(column, row_idx):
+    column_idx = column + str(row_idx)
+    return column_idx
+
+
+mecab = Mecab()
+# test용으로 simple의 한글 먼저 받아와서 돌리기
+ko_list = call_tagMT_ko_simple_lan('ko')
 for _ in ko_list:
-    print(_, '\n')
-# print(raw_data['ko'].tolist())
+    a_idx = excel_index_creator('A', row_idx)
+    worksheet.write(a_idx, _)
+
+    b_idx = excel_index_creator('B', row_idx)
+    pos = str(mecab.pos(_))
+    worksheet.write(b_idx, pos)
+    
+    row_idx += 1
+    
+workbook.close()
