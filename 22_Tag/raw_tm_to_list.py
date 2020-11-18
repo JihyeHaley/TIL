42ae96ae70930b9b07517c9a32ea45f5ec43120f
JihyeHaley <ohhojh@gmail.com>
2020-11-18 16:11:43 +0900

    succeed to extract idx and write in excel

--- 22_Tag/raw_tm_to_list.py
+++ 22_Tag/raw_tm_to_list.py
@@ -23,26 +23,28 @@ def call_tagMT_ko_total():
 ############### language 선택 to list ###############
 def call_tagMT_ko_simple_lan(lan):
     tm_simple_raw_data = pd.read_excel('./tagMT_ko_simpl.xlsx')
+    path_simple_list = tm_simple_raw_data['path'].tolist()
     ko_simple_list = tm_simple_raw_data['ko'].tolist()
     en_simple_list = tm_simple_raw_data['en'].tolist()
     ja_simple_list = tm_simple_raw_data['ja'].tolist()
     
     if lan == 'ko':
-        return ko_simple_list
+        return path_simple_list, ko_simple_list
     elif lan == 'en':
-        return en_simple_list
+        return path_simple_list, en_simple_list
     elif lan == 'ja':
-        return ja_simple_list
+        return path_simple_list, ja_simple_list
 
 def call_tagMT_ko_total_lan(lan):
     tm_total_raw_data = pd.read_excel('./tagMT_ko_total.xlsx')
+    path_total_list = tm_total_raw_data['path'].tolist()
     ko_total_list = tm_total_raw_data['ko'].tolist()
     en_total_list = tm_total_raw_data['en'].tolist()
     ja_total_list = tm_total_raw_data['ja'].tolist()
 
     if lan == 'ko':
-        return ko_total_list
+        return path_total_list, ko_total_list
     elif lan == 'en':
-        return en_total_list
+        return path_total_list, en_total_list
     elif lan == 'ja':
-        return ja_total_list
+        return path_total_list, ja_total_list
