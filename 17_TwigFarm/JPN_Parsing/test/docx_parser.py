f120e4d9c10ed1ef30a43a6f2d7125b16dbcc0ed
JihyeHaley <ohhojh@gmail.com>
2020-12-11 22:45:59 +0900

    japanese parsing

--- 17_TwigFarm/JPN_Parsing/test/docx_parser.py
+++ 17_TwigFarm/JPN_Parsing/test/docx_parser.py
@@ -11,6 +11,10 @@ from datetime import datetime
 
 import mammoth
 
+def excel_index_creator(colum, row_idx):
+    colum_idx = colum + str(row_idx)
+    return colum_idx
+
 
 stop_word = []
 
@@ -25,191 +29,107 @@ def docx_to_raw_text(file_name):
 
     contents_prep = []
     for line in contents:
-        if '章. ' in line:
-            each_line = line.split('.')
+        if '。' in line:
+            each_line = line.split('。')
             for each_each_line in each_line:
-                contents_prep.append(each_each_line)
+                contents_prep.append(each_each_line+'。')
         else:
             each_line = line.strip()
             contents_prep.append(each_line)
 
     return contents_prep
-file_name = '1. InnoRules Install Guide of Rule Builder.docx'
-raw_data = docx_to_raw_text(file_name)
-print(raw_data)
+
+f_1 = '1. InnoRules Install Guide of Rule Builder.docx'
+f_2 = '2. InnoRules User\'s Guide of Rule Builder(JP).docx'
+f_3 = '3. InnoRules Installation and Operation Guide v7.2(JP).docx'
+f_4 = '4. InnoRules Application Programming Interface Guide(JP).docx'
+f_6 = '6. InnoProduct Repository Specification.docx'
+f_7 = '7. InnoProduct Rule Service Guide of Product Builder.docx'
+f_8 = '8. InnoProduct User\'s Guide of Product Builder-JP-20200323.docx'
+f_9 = '9. InnoProduct Installation and Operation Guide of Product Builder.docx'
 
 
-workbook = xlsxwriter.Workbook('./1번_데이터.xlsx') # _mustbessossc
-worksheet = workbook.add_worksheet()
-worksheet.write('A1', 'Japanese')
+# 하나씩
+def find_which_file():
+    which_file = str(input())
+    path = './jap/'
+    file_name = ''
+    if which_file == 'f_1':
+        file_name = path + f_1
+    elif which_file == 'f_2':
+        file_name = path + f_2
+    elif which_file == 'f_3':
+        file_name = path + f_3
+    elif which_file == 'f_4':
+        file_name = path + f_4
+    elif which_file == 'f_6':
+        file_name = path + f_6
+    elif which_file == 'f_7':
+        file_name = path + f_7
+    elif which_file == 'f_8':
+        file_name = path + f_8
+    elif which_file == 'f_9':
+        file_name = path + f_9
+
+    return file_name
+    
+# file_name = find_which_file()
 
 
-def excel_index_creator(colum, row_idx):
-    colum_idx = colum + str(row_idx)
-    return colum_idx
+# 파일에 path 
+def file_name_with_path():
+    file_lists = list()
+    path = './jap/'
+    file_lists.append(path + f_1)
+    file_lists.append(path + f_2)
+    file_lists.append(path + f_3)
+    file_lists.append(path + f_4)
+    file_lists.append(path + f_6)
+    file_lists.append(path + f_7)
+    file_lists.append(path + f_8)
+    file_lists.append(path + f_9)
 
-row_idx = 2 
-for raw_sent in raw_data:
-    idx = excel_index_creator('A', row_idx)
-
-    if raw_sent == '':
-        continue
-    else:
-        input_raw_sent = raw_sent
-
-    worksheet.write(idx, input_raw_sent)
-    print(raw_sent)
-    print(row_idx)
-    row_idx += 1
-
-workbook.close()
-
-# # 파일을 돌리는 해당 경로에 결과 엑셀 생성
-# def docx_mix_to_excel(docx_mix_files, sub_path):
-#     if len(docx_mix_files) == 0:
-#         print('''-----------------------------------------------------------
-#         DOCX Mix 파일 없습니다.
-#         ''')
-#     else:
-#         print('''-----------------------------------------------------------
-#         DOCX Mix  작업 시작합니다.
-#         ''')
-#         timestamp = datetime.now().strftime('%m%d%H%M')
-        
-#         # # path별 문서 list를 가져옴
-#         # docx_name_lists = get_filename_list(file_path, '.docx')
-        
-        
-#         workbook = xlsxwriter.Workbook('./results/' + sub_path  + '/' + sub_path  +  '_docx_mix_'  + timestamp +'.xlsx') # _mustbessossc
-#         worksheet = workbook.add_worksheet()
-#         worksheet.write('A1', 'Raw Data')
-#         worksheet.write('B1', 'Japanese')
-
-#         row_idx = 2
-
-#         completed_log = open(f'./results/'  + sub_path  + '/' + sub_path + '_log_docx_path_' + timestamp + '.txt', "w+")
-#         docx_mix_word_list_friend = list()
-#         # for timer
-#         start = timeit.default_timer()
-
-        
-
-#         filename = docx_mix_files
-
-#         try:
-#             # Word to raw text
-#             contents = docx_to_raw_text(filename, stop_word)
-
-#             ## 각 문서종류별 폴더 안에 파싱된 txt 파일 추가, 폴더가 없으면 생성
-#             # pathlib.Path('./' + sub_dir + '_txt').mkdir(parents=True, exist_ok=True)
-#             # txt_path = os.path.join(f'./' + sub_dir + '_txt/', filename + ".txt")
-#             # all_words = open(txt_path, "w+")
-
-#             kor_raw_list = []
-#             kor_sent_list = []
-
-#             for lines in contents:
-
-#                 each_line = lines.strip()
-
-#                 # Kor or Kor/Eng
-#                 if not mustEnglish(each_line) and isKorean(each_line):
-
-#                     # Kor and Enl - split (영어 두단어 이상 기준)
-#                     if isEnglish(each_line):
-#                         kor_and_eng = split_kor_eng(each_line)
-#                         for both_lang in kor_and_eng:
-#                             if both_lang.strip() and mustEnglish(both_lang):
-#                                 eng_raw_list.append(both_lang)
-#                             elif both_lang.strip() and not mustEnglish(both_lang):
-#                                 kor_raw_list.append(both_lang)
-
-#                     else:
-#                         # 한글 or 영어없는 공백,숫자,특수문자
-#                         kor_raw_list.append(each_line)
+    return file_lists
 
 
-#             # Remove empty string
-#             kor_raw_list = list(filter(None, kor_raw_list))
-        
-
-            
-#             # raw paragraphs -> sentence tokenize
-#             for ko_lines in kor_raw_list:
-#                 kor_sents = sent_tokenize(regex_cleaner(ko_lines))  # nltk
-
-#                 for ko_sent in kor_sents:
-                    
-#                     # 토큰화된 문장에 한/영이 없으면 empty string = no insert
-#                     if neitherKoNorEn(ko_sent):
-#                         continue
-
-#                     kor_sent_list.append(ko_sent)
-
-#                 # 중복 제거
-#             kor_sent_list_full = list()
-#             for sent in kor_sent_list:
-#                 if sent not in kor_sent_list_full:
-#                     kor_sent_list_full.append(sent)
-#             # 총 몇줄인지 확인
-#             total_cnt += len(kor_sent_list_full)
-#             # print(f'{total_idx} - {len(kor_sent_list)}')
-
-#             for idx, raw_sent in enumerate(kor_sent_list_full):
-#                 # 한글, 영어가 같이 있는게 아니라면 건너뛰기
-#                 if isSentKoreanAndEnglish(raw_sent) == False:
-#                     continue
-
-#                 # A. Path 쓰기
-#                 a_idx =excel_index_creator('A', row_idx)
-#                 path = each_file.split('/')
-#                 path =  f'{path[-3]}/{path[-2]}/{path[-1]}/{idx}'
-#                 worksheet.write(a_idx, path)
-
-#                 # raw _sent 형태소 분석 시작
-#                 # B. Raw Sent 쓰기
-#                 b_idx =excel_index_creator('B', row_idx)
-#                 worksheet.write(b_idx, raw_sent)
-#                 ko_words, en_words= find_pattern_show_words(raw_sent)
-                
 
 
-#                 for j in range(len(ko_words)):
-#                     # 중복 방지
-#                     if [ko_words[j], en_words[j]] in docx_mix_word_list_friend:
-#                         continue
-#                     else:
-#                         docx_mix_word_list_friend.append([ko_words[j], en_words[j]])
-#                         # D의 개수가 1개면 skip
-#                         en_words[j] = en_words[j].strip(' ')
-#                         if skip_mored_word(en_words[j]) == True:
-#                             continue
-#                         else:
-#                             # C.  ko_word 쓰기
-#                             c_idx =excel_index_creator('C', row_idx)
-#                             worksheet.write(c_idx, ko_words[j])
 
+def parsing_docx_at_once():
 
-#                             # D.  en_word 쓰기
-#                             d_idx = excel_index_creator('D', row_idx)
-#                             worksheet.write(d_idx, en_words[j])
-                            
-#                             row_idx += 1
+    start = timeit.default_timer()
 
-#             # Write Complete log
-#             path = each_file.split('/')
-#             path =  f'{path[-3]}/{path[-2]}/{path[-1]}'
-#             completed_log.write('[DONE READING]' + path + '\n')
-            
+    file_lists = file_name_with_path()
+    total_row_cnt = 0
+    completed_log = open(f'./docx_file' + '.txt', "w+")
+    for idx, file_name in enumerate(file_lists):
+        raw_data = docx_to_raw_text(file_name)
+        # print(raw_data)
+        print('#'*30)
+        print(f'{idx+1}. file_name: {file_name}')
+        print(f'raw data: {len(raw_data)}줄')
+        workbook = xlsxwriter.Workbook('./parsed/' + file_name[6:]+'_.xlsx') 
+        print('it works')
+        worksheet = workbook.add_worksheet()
 
-#         # Write Error log file when docx -> txt
-#         except Exception as e:
-#             completed_log.write('[ERROR MESSAGE]' + each_file + str(e) + '\n')
-#             print('[ERROR]' + filename + str(e) +' got ERROR! \n')
+        row_idx = 1
+        for raw_sent in raw_data:
+            idx = excel_index_creator('A', row_idx)
 
-        
-#         workbook.close()
-#         completed_log.close()
-#         stop = timeit.default_timer()
-#         print(' DOCX_Mix =====> Raw text to excel DONE (Running Time: ', stop - start, "sec.)")
-#         print(' Total cnt: ', str(total_cnt))
+            if raw_sent == '':
+                continue
+            else:
+                input_raw_sent = raw_sent
+            worksheet.write(idx, input_raw_sent)
+            row_idx += 1
+        print(f'excel idx: {row_idx}줄')
+        total_row_cnt += row_idx
+        completed_log.write(f'######################\n{file_name}\n엑셀:{row_idx}/\t{len(raw_data)}\n')
+        workbook.close()
+
+    stop = timeit.default_timer()    
+    completed_log.close()
+    print(f' DOCX_=====> Raw text to excel DONE (Running Time: {stop - start}sec.)')
+    print(f'total_cnt: {total_row_cnt}')
+
+parsing_docx_at_once()
