e1c20d0d9d9ec4843f6e0f0c9639280a35edc60e
JihyeHaley <ohhojh@gmail.com>
2021-02-09 17:58:27 +0900

    today I worked

--- 17_TwigFarm/data_similarity_API/api_test/papago_api_test.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import urllib.request
-
-# CLIENT_ID = '844ikga37u'
-client_id = '844ikga37u'
-# CLIENT_SECRET = 'WyPlwZQVOGSKbov3xVzdKKuTUNHiruagKYcWBrvs'
-client_secret = 'WyPlwZQVOGSKbov3xVzdKKuTUNHiruagKYcWBrvs'
-
